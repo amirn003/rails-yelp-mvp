32b5516ff1f1d22527ea59938b28cd9d46414bdf
amirn003 <amir.naar@gmail.com>
2023-07-28 19:47:54 +0200

    add reviews controller, forms, links

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,5 +5,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(params_set)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_set
+    params.require(:review).permit(:content, :rating)
   end
 end
